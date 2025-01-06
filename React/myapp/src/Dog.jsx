cc3cf2ed85ae74ee0ca0cb6185d3e57cad069c87
Yogeshtn05 <yogeshtn.22ece@kongu.edu>
2025-01-06 09:15:50 +0530

    Backend Demo

--- React/myapp/src/Dog.jsx
+++
@@ -1,9 +0,0 @@
-import Ant from './Ant.jsx'
-export default function Dog(props){
-    return(
-        <>
-        <h1>Father : {props.name}</h1>
-        <Ant name={props.name}/>
-        </>
-    )
-}
